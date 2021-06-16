82c3019a3a5e8b9c8fa9a3bd1a472045918b0f07
guilherme-inkotte <gui.inkotte@hotmail.com>
2021-06-15 22:09:06 -0300

    Estilizando a página inicial e a de cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -5,12 +5,18 @@ const birthdateInput = document.getElementById('birthdate');
 const maleInput = document.getElementById('male');
 const femaleInput = document.getElementById('female');
 const noneInput = document.getElementById('none');
+const submitButton = document.getElementById('submit-button');
 
-function submit() {
+console.log(submitButton)
+
+function submit(e) {
+    e.preventDefault();
     if(!nameInput.value) return alert('Você deve preencher o nome')
     if(!emailInput.value) return alert('Você deve preencher o email')
     if(!passwordInput.value) return alert('Você deve preencher a senha')
     if(!birthdateInput.value) return alert('Você deve preencher a data de nascimento')
     if(!maleInput.checked && !femaleInput.checked && !noneInput.checked) return alert('Você deve preencher o gênero')
-    window.location.href = 'autenticado.html'
+    window.location.href = 'authenticated.html'
 };
+
+submitButton.addEventListener('click', submit)
